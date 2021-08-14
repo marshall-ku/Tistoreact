fc1945adf1cccd051ed76673d9fd15e3b9b69279
marshall-ku <marshall@kakao.com>
2021-08-14 17:52:27 +0900

    Remove prettierrc and reformat files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>hi</h1>
+        </div>
+    );
 }
 
 export default App;
